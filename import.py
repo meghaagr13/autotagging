30c5cce38f4acaf00469808dccdd889618198dcf
meghaagr13 <agarwalmegha1308@gmail.com>
2017-06-30 01:34:32 +0530

    \n error resolved

--- import.py
+++ import.py
@@ -1,7 +1,7 @@
 import lib.code_extract_specific_problem as code_extract
 import sys
 import getopt
-
+from bs4 import BeautifulSoup 
 argc = len(sys.argv)
 argv = sys.argv
 
@@ -19,4 +19,6 @@ else:
 	print(page_number)
 	for i in range(1,int(page_number)):
 		submission_id = submission_id + code_extract.get_submission_id(contest_id,problem_char,page_number)
+	submission_id=[el.replace('\n', '') for el in submission_id]
+#	submission_id=filter(lambda x: x!='\n', submission_id)
 	print(submission_id)	
